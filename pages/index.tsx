9a7e2ef9bb750821fb32f43436e065f36ac2a286
subhamay2002 <subhamaynath2002@gmail.com>
2022-08-10 19:36:29 +0530

    setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,8 +24,18 @@ const Home: NextPage = () => {
 
   const [data, setData] = useState([]);
   useEffect(() => {
-
+    fetch('https://coinranking1.p.rapidapi.com/coins', {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+        'x-rapidapi-key': '323c7e8ec9msh65adfc7e90148c8p11cce2jsn2fbbd6e8d26e'
+      }
+    })
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(err => console.log(err));
   },[]);
+  console.log(data)
   
   
    
